483715611a55d760ff30c1bd013c0e5afaae7970
sunwanqi <swq-1993@163.com>
2018-11-22 14:05:14 +0800

    first init

---
+++ cmake-build-debug/CMakeFiles/yuanfudao_oneMore.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yuanfudao_oneMore.dir/main.cpp.o"
+  "yuanfudao_oneMore.pdb"
+  "yuanfudao_oneMore"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yuanfudao_oneMore.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
